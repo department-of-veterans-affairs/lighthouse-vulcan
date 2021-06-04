aeb41745e720b68af88f1bafa935f1588110033a
Peter Branam-Lefkove <peter.branam-lefkove@libertyits.com>
2021-06-04 17:22:45 -0400

    .

--- src/test/java/gov/va/api/lighthouse/vulcan/fugazi/FugaziEntity.java
+++ src/test/java/gov/va/api/lighthouse/vulcan/fugazi/FugaziEntity.java
@@ -6,12 +6,12 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
-import lombok.NonNull;
 
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
@@ -21,10 +21,10 @@ import lombok.NonNull;
 @Entity
 @Table(name = "FOO")
 public class FugaziEntity {
-  @Id @GeneratedValue @Column @EqualsAndHashCode.Include long id;
-  @Column @NonNull String name;
+  @Id @GeneratedValue @Column @NotNull @EqualsAndHashCode.Include long id;
+  @Column @NotNull String name;
   @Column String food;
-  @Column @NonNull Instant date;
+  @Column @NotNull Instant date;
   @Column long millis;
   @Column String payload;
 }
